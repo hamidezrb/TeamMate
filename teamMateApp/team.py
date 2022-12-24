271ff8760f3405e2bb0ad8a4c21fa03ee136f06b
hamidezrb <hamide.queen@gmail.com>
2022-12-25 00:49:55 +0330

    edit README

--- teamMateApp/team.py
+++ teamMateApp/team.py
@@ -1,4 +1,5 @@
 from .models import Participants
+from django.utils import timezone
 
 class team:
     def get_teams(request,teams):
@@ -13,6 +14,6 @@ class team:
             participantsNO = Participants.objects.filter(team = team).count()
             remains = team.participantsNO - participantsNO
             list_team.append({ 'participants':participants ,'team_id' : team.id, 'title' : team.title ,'content' :team.content , 'startdate' : team.startdate,
-                            'finishdate' : team.finishdate , 'image' :team.image.url , 'createuser_image' : team.user.image.url, 'user_id' : team.user.id, 'authenticated' : request.user.is_authenticated,
+                            'finishdate' : team.finishdate , 'isFinished' : True if team.finishdate < timezone.now() else False , 'image' :team.image.url , 'createuser_image' : team.user.image.url, 'user_id' : team.user.id, 'authenticated' : request.user.is_authenticated,
                             'createdate' : team.createdate.time(), 'participantsNO' : team.participantsNO , 'remains' : remains , 'viewMore' : viewMore, 'owner' : True if request.user.id == team.user.id else False })
         return list_team
